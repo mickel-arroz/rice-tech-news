aa87fa4a248242bb0870c92aef59c5e7fea4c52c
Mickel Arroz <111148461+mickel-arroz@users.noreply.github.com>
2026-08-09 03:11:11 +0100

    feat: :sparkles: Add Open Graph + Delete old favicon.ico

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rice-tech-news.vercel.app',
   integrations: [react()],
   adapter: vercel(),
   vite: {
